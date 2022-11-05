a2e8b9a1a9e038f1c5c3ca495380392d202058b0
takagisatoshi <siroikutu@gmail.com>
2022-11-05 16:40:53 +0900

    update encode&decode

--- src/page/URLEncoderDecoder.tsx
+++ src/page/URLEncoderDecoder.tsx
@@ -7,8 +7,8 @@ const URLEncoderDecoder = (props: PropsWithChildren<any>) => {
                                     leftPlaceholder={"encode string https://www.google.com/search?q=こんにちは"}
                                     rightTitle={"Decode"}
                                     rightPlaceholder={"decode string https://www.google.com/search?q=%E3%81%93%E3%82%93%E3%81%AB%E3%81%A1%E3%81%AF"}
-                                    onLeftToRight={(arg) => encodeURI(arg)}
-                                    onRightToLeft={(arg) => decodeURI(arg)}
+                                    onLeftToRight={(arg) => encodeURIComponent(arg)}
+                                    onRightToLeft={(arg) => decodeURIComponent(arg)}
     >
     </ConvertTemplateOneToOne>
 }
